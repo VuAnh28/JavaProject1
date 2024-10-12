ba282c302e818e53cac9b7fee0c1becdff16a3a4
ndang <ndang2319@gmail.com>
2024-10-12 13:49:50 +0700

    feat: Add DTO classes for Customer and Staff with common User properties

---
+++ koi-pond-backend/src/main/java/org/group/koipondbackend/dto/CustomerDTO.java
@@ -0,0 +1,14 @@
+package org.group.koipondbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class CustomerDTO extends UserDTO {
+    private String address;
+}
